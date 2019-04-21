9a53f3bae82a238a7a34b836c2923851e8a2b974
Howard Ricketts <molorosh@gmail.com>
2019-04-21 22:47:04 +0100

    added the first version of the website

---
+++ docs/precache-manifest.65e4edb77d93141ce46522ec550bbbbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b772089f4ee911ca9c5a",
+    "url": "/ReactControls/static/js/runtime~main.5a5f3924.js"
+  },
+  {
+    "revision": "f27c7b7c7eb6cc1bc451",
+    "url": "/ReactControls/static/js/main.6fd7798a.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/ReactControls/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "f27c7b7c7eb6cc1bc451",
+    "url": "/ReactControls/static/css/main.a6a3a193.chunk.css"
+  },
+  {
+    "revision": "123c868a62da2127e4393d8d315ed5f9",
+    "url": "/ReactControls/index.html"
+  }
+];
